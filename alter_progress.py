3f020777e28de6eaebfa15cc947d58916495a8bc
waseemakram786 <58878927+waseemakram786@users.noreply.github.com>
2019-12-14 17:13:03 +0530

    used sys.format_time instead of format_pico_time
    format_pico_time was not supporting the MySQL 5.7 version, sys.format_time  is compatible with both 5.7 and 8.0.

--- alter_progress.py
+++ alter_progress.py
@@ -1,15 +1,15 @@
 def alter_progress(session):
     query = session.sql("""SELECT stmt.THREAD_ID, stmt.SQL_TEXT, stage.EVENT_NAME AS State,
-                   stage.WORK_COMPLETED, stage.WORK_ESTIMATED,
-                   lpad(CONCAT(ROUND(100*stage.WORK_COMPLETED/stage.WORK_ESTIMATED, 2),"%"),12," ") 
-                   AS CompletedPct, 
-                   lpad(format_pico_time(stmt.TIMER_WAIT), 10, " ") AS StartedAgo,
-                   current_allocated Memory           
-            FROM performance_schema.events_statements_current stmt                
-            INNER JOIN sys.memory_by_thread_by_current_bytes mt  
-                    ON mt.thread_id = stmt.thread_id 
-            INNER JOIN performance_schema.events_stages_current stage 
-                    ON stage.THREAD_ID = stmt.THREAD_ID""")
+						stage.WORK_COMPLETED, stage.WORK_ESTIMATED,
+						lpad(CONCAT(ROUND(100*stage.WORK_COMPLETED/stage.WORK_ESTIMATED, 2),"%"),12," ")
+ 						AS CompletedPct,
+ 						lpad(sys.format_time(stmt.TIMER_WAIT), 10, " ") AS StartedAgo,
+						current_allocated Memory 
+			FROM performance_schema.events_statements_current stmt 
+			INNER JOIN sys.memory_by_thread_by_current_bytes mt 
+					ON mt.thread_id = stmt.thread_id 
+			INNER JOIN performance_schema.events_stages_current stage 
+					ON stage.THREAD_ID = stmt.THREAD_ID""")
 
     result = query.execute()
     report = [result.get_column_names()]
